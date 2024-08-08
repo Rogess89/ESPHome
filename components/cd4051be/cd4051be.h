7b4fab77503161d363da8f29ae78806e6fd48ca5
Rogess89 <rogess@hotmail.de>
2024-08-08 16:53:51 +0200

    Add files via upload

---
+++ components/cd4051be/cd4051be.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/components/sensor/sensor.h"
+#include "esphome/components/voltage_sampler/voltage_sampler.h"
+
+namespace esphome {
+namespace cd4051be {
+
+class CD4051BEComponent : public Component {
+ public:
+  /// Set up the internal sensor array.
+  void setup() override;
+  void dump_config() override;
+  float get_setup_priority() const override;
+
+  /// setting pin active by setting the right combination of the four multiplexer input pins
+  void activate_pin(uint8_t pin);
+
+  /// set the pin connected to multiplexer control pin a
+  void set_pin_a(GPIOPin *pin) { this->pin_a_ = pin; }
+  /// set the pin connected to multiplexer control pin b
+  void set_pin_b(GPIOPin *pin) { this->pin_b_ = pin; }
+  /// set the pin connected to multiplexer control pin c
+  void set_pin_c(GPIOPin *pin) { this->pin_c_ = pin; }
+  /// set the pin connected to multiplexer control pin inh
+  void set_pin_inh(GPIOPin *pin) { this->pin_inh_ = pin; }
+
+  /// set the delay needed after an input switch
+  void set_switch_delay(uint32_t switch_delay) { this->switch_delay_ = switch_delay; }
+
+ private:
+  GPIOPin *pin_a_;
+  GPIOPin *pin_b_;
+  GPIOPin *pin_c_;
+  GPIOPin *pin_inh_;
+  /// the currently active pin
+  uint8_t active_pin_;
+  uint32_t switch_delay_;
+};
+
+class CD4051BESensor : public sensor::Sensor, public PollingComponent, public voltage_sampler::VoltageSampler {
+ public:
+  CD4051BESensor(CD4051BEComponent *parent);
+
+  void update() override;
+
+  void dump_config() override;
+  /// `HARDWARE_LATE` setup priority.
+  float get_setup_priority() const override;
+  void set_pin(uint8_t pin) { this->pin_ = pin; }
+  void set_source(voltage_sampler::VoltageSampler *source) { this->source_ = source; }
+
+  float sample() override;
+
+ protected:
+  CD4051BEComponent *parent_;
+  /// The sampling source to read values from.
+  voltage_sampler::VoltageSampler *source_;
+
+  uint8_t pin_;
+};
+}  // namespace cd4051be
+}  // namespace esphome
---
+++ components/cd4051be/sensor.py
@@ -0,0 +1,55 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, voltage_sampler
+from esphome.const import (
+    CONF_ID,
+    CONF_SENSOR,
+    CONF_NUMBER,
+    ICON_FLASH,
+    UNIT_VOLT,
+    STATE_CLASS_MEASUREMENT,
+    DEVICE_CLASS_VOLTAGE,
+)
+from . import cd4051be_ns, CD4051BEComponent
+
+DEPENDENCIES = ["cd4051be"]
+
+CD4051BESensor = cd4051be_ns.class_(
+    "CD4051BESensor",
+    sensor.Sensor,
+    cg.PollingComponent,
+    voltage_sampler.VoltageSampler,
+)
+
+CONF_CD4051BE_ID = "cd4051be_id"
+
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        CD4051BESensor,
+        unit_of_measurement=UNIT_VOLT,
+        accuracy_decimals=3,
+        device_class=DEVICE_CLASS_VOLTAGE,
+        state_class=STATE_CLASS_MEASUREMENT,
+        icon=ICON_FLASH,
+    )
+    .extend(
+        {
+            cv.GenerateID(CONF_CD4051BE_ID): cv.use_id(CD4051BEComponent),
+            cv.Required(CONF_NUMBER): cv.int_range(0, 7),
+            cv.Required(CONF_SENSOR): cv.use_id(voltage_sampler.VoltageSampler),
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+)
+
+
+async def to_code(config):
+    parent = await cg.get_variable(config[CONF_CD4051BE_ID])
+
+    var = cg.new_Pvariable(config[CONF_ID], parent)
+    await sensor.register_sensor(var, config)
+    await cg.register_component(var, config)
+    cg.add(var.set_pin(config[CONF_NUMBER]))
+
+    sens = await cg.get_variable(config[CONF_SENSOR])
+    cg.add(var.set_source(sens))
